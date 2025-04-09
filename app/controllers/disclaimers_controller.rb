6559a17647cb6ad4e5adf32a1a402484cdc45a7f
gus1000 <gramirez644@gmail.com>
2025-04-09 11:59:56 -0700

    added json end point, formatting

--- app/controllers/disclaimers_controller.rb
+++ app/controllers/disclaimers_controller.rb
@@ -15,6 +15,12 @@ class DisclaimersController < ApplicationController
   end
 
   def show 
+
+    respond_to do |format|
+      format.html 
+      format.json {render json: @disclaimer}
+    end
+
   end
 
   def create 
