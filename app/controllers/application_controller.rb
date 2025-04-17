afdb97c7b0fe6a445e2bfa56a2477bf8a0a264e2
gus1000 <gramirez644@gmail.com>
2025-04-16 19:25:53 -0700

    tested login , resolved login issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  before_action :set_csrf_cookie
+
+  protect_from_forgery with: :null_session
+
 
   protected
 
@@ -9,11 +13,12 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username] )
+  
 
+  private 
 
+  def set_csrf_cookie
+    cookies['CSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
   end
 
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,6 +4,4 @@
 #   secure: Rails.env.production? || Rails.env.development?
 
 
-Rails.application.config.session_store :cookie_store,
-  key: "_legal_project_session",
-  same_site: :none
+Rails.application.config.session_store :cookie_store, key: '_legal_project_session'
